b28b2c5f9b37e0d3b76221e82c5c902d3846d808
Krishna <krishnarathi341@gmail.com>
2023-02-02 20:53:25 +0530

    completed ApI fetching logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Weather from "./Components/Weather";
 
 function App() {
-  return <h1>This is App.js</h1>;
+  const [long, setLong] = useState(null);
+  const [lat, setLat] = useState(null);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      if (long != null && lat != null) {
+        await fetch(
+          `${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_API_KEY}`
+        )
+          .then((res) => res.json())
+          .then((result) => {
+            setData(result);
+            console.log(result);
+          });
+      }
+    };
+
+    fetchData();
+  }, [long, lat]);
+
+  return (
+    <>
+      <div className="App">
+        {typeof data.main != "undefined" ? (
+          <Weather weatherdata={data} />
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/Weather.js
@@ -0,0 +1,5 @@
+const Weather = () => {
+  return <h1>Hello from Weather</h1>;
+};
+
+export default Weather;
